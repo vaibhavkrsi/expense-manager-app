6e76cdc426c0a0d53555207ea3ea6d4b5766bd12
vaibhavkrsi <hyperv117@gmail.com>
2023-04-15 13:09:24 +0530

    Second commit

---
+++ src/components/context/AppReducer.js
@@ -0,0 +1,17 @@
+//redux is for state changes basically changes we do want in an application
+export default (state,action)=>{
+    switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return{
+                ...state,
+                transactions : state.transactions.filter(transaction=>transaction.id !== action.payload)
+            } 
+        case'ADD_TRANSACTION': 
+            return{ 
+                ...state, 
+                transactions:  [action.payload , ...state.transactions]  
+            }
+        default:
+            return state;
+    }
+} 
